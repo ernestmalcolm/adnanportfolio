50709446e50a555c2fc2e91403eb4f07c3d468f2
Ernest Mwinchumu <gmem200197@gmail.com>
2023-01-29 02:59:35 +0300

    Updated to Nextjs13 and added Tailwindcss

---
+++ app/contact/page.js
@@ -0,0 +1,5 @@
+import '../globals.css'
+
+export default function Contact() {
+  return <p className="underline">Hello. Call me</p>;
+}
---
+++ app/page.js
@@ -0,0 +1,5 @@
+import './globals.css'
+
+export default function Page() {
+  return <p className="underline">Hello. I'm Adrian Mvungi</p>;
+}
